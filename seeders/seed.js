61dae3013166a6cb3b7e546e3c7852d5384d7802
Amy Hwang <728amyh@gmail.com>
2022-01-19 14:38:20 -0500

    working be

--- seeders/seed.js
+++ seeders/seed.js
@@ -4,6 +4,7 @@ const db = require('../models');
 mongoose.connect('mongodb://localhost/workout', {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 const workoutSeed = [
